44afa67fffdb00acbb3e9979e4b9498020bb9332
Tammer Saleh <tammer@superorbit.al>
2021-01-27 09:12:46 -0800

    better post pull debugging

--- post-pull.sh
+++ post-pull.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
+trap 'echo "$0 failed at line $LINENO: $BASH_COMMAND"' ERR
+[[ -v DEBUG ]] && set -x
+
 echo "Syncing vim plugins"
 ~/.vim/plug-install.sh
 
