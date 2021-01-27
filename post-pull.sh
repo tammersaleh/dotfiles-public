10af3937ddbebb1066ee6e960fd7142103a8e675
Tammer Saleh <tammer@superorbit.al>
2021-01-27 09:13:44 -0800

    yolo

--- post-pull.sh
+++ post-pull.sh
@@ -6,8 +6,9 @@ shopt -s inherit_errexit
 trap 'echo "$0 failed at line $LINENO: $BASH_COMMAND"' ERR
 [[ -v DEBUG ]] && set -x
 
+echo "Installing Brewfile"
+brew bundle install --cleanup --force
+
 echo "Syncing vim plugins"
 ~/.vim/plug-install.sh
 
-echo "Installing Brewfile"
-brew bundle install --cleanup --force
