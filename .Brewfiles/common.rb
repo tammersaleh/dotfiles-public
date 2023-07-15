5688c3a563f5561e0a4087d65ceed74c36c9743a
Tammer Saleh <tammer@superorbital.io>
2023-07-15 15:18:24 -0700

    Lesspipe utilities

--- .Brewfiles/common.rb
+++ .Brewfiles/common.rb
@@ -12,17 +12,19 @@ brew "awscli"                        # Official Amazon AWS command-line interfac
 brew "bash"                          # Bourne-Again SHell, a UNIX command interpreter
 brew "bash-completion@2"             # Common bash-completion libraries
 brew "bat"                           # Clone of cat(1) with syntax highlighting and Git integration
+brew "bat-extras"
 brew "cmake"                         # Cross-platform make
 brew "coreutils"                     # GNU File, Shell, and Text utilities
 brew "ctags"                         # Reimplementation of ctags(1)
 brew "diff-so-fancy"                 # Good-lookin' diffs with diff-highlight and more
 brew "direnv"                        # Load/unload environment variables based on $PWD
 brew "doppler" # doppler.com
+brew "elinks"                        # For lesspipe
 brew "entr"                          # Run arbitrary commands when files change
 brew "exa"
 brew "exiftool"                      # Perl lib for reading and writing EXIF metadata
 brew "ffmpeg"                        # Play, record, convert, and stream audio and video
-brew "fzf"  # used by vim finder
+brew "fzf"                           # used by vim finder
 brew "gh"                            # GitHub command-line tool
 brew "git"                           # Distributed revision control system
 brew "git-crypt"                     # Enable transparent encryption/decryption of files in a git repo
@@ -34,12 +36,14 @@ brew "gron"                          # Make JSON greppable
 brew "heroku"                        # Add GitHub support to git on the command-line
 brew "httpie"                        # User-friendly cURL replacement (command-line HTTP client)
 brew "hub"  # Used for campus
+brew "id3v2"                         # For lesspipe
 brew "imagemagick"                   # Tools and libraries to manipulate images in many formats
 brew "jq"                            # Lightweight and flexible command-line JSON processor
 brew "lazygit"
 brew "lesspipe"                      # Input filter for the pager less
 brew "libpq", force: true # Used to get libpq and force linked to get psql cli in PATH
 brew "libxslt"                       # C XSLT library for GNOME
+brew "mediainfo"                     # For lesspipe
 brew "netlify/netlifyctl/netlifyctl" # CLI to interact with netlify.com
 brew "node"                          # Platform built on V8 to build network applications
 brew "pandoc"                        # Swiss-army knife of markup format conversion
@@ -61,26 +65,12 @@ brew "tree"                          # Display directories as trees (with option
 brew "vim"                           # Vi 'workalike' with many additional features
 brew "watch"                         # Executes a program periodically, showing output fullscreen
 brew "wget"                          # Internet file retriever
+brew "xpdf"                          # For lesspipe
 brew "xz"                            # General-purpose data compression with high compression ratio
 brew "yarn"
 brew "yq"
 brew "zlib"                          # General-purpose lossless data-compression library
 brew "zsh"
-
-# brew "cfssl"                         # CloudFlare's PKI toolkit
-# brew "consul"                        # Tool for service discovery, monitoring and configuration
-# brew "elinks"                        # Text mode web browser
-# brew "flyctl"                        # Fly.io
-# brew "helm"                          # The Kubernetes package manager
-# brew "kube-ps1"                      # Kubernetes prompt info for bash and zsh
-# brew "kubectx"                       # Tool that can switch between kubectl contexts easily and create aliases
-# brew "kubernetes-cli"                # Kubernetes command-line interface
-# brew "links"                         # Lynx-like WWW browser that supports tables, menus, etc.
-# brew "lynx"                          # Text-based web browser
-# brew "mosh"                          # Remote terminal application
-# brew "noti"                          # Trigger notifications when a process completes
-# brew "stern"                         # Tail multiple Kubernetes pods & their containers
-# brew "vale"                          # Prose Linter https://docs.errata.ai/vale
 
 cask "font-fira-code"
 
