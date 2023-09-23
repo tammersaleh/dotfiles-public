dbd37a6a99c4b7f7ab12b8583ad558143a5455de
Tammer Saleh <tammer@superorbital.io>
2023-09-23 14:35:39 -0700

    Stuff

--- .Brewfiles/Darwin.rb
+++ .Brewfiles/Darwin.rb
@@ -11,10 +11,10 @@ brew "aspell"                     # Spell checker with better logic than ispell
 brew "autoconf"                   # Automatic configure script builder
 brew "bzip2"                      # Freely available high-quality data compressor
 brew "diffutils"
+brew "gawk"
 brew "gettext", link: true        # GNU internationalization (i18n) and localization (l10n) library
 brew "gnu-sed"
 brew "gnu-tar"                    # GNU version of the tar archiving utility
-brew "gawk"
 brew "libnotify"                  # Library that sends desktop notifications to a notification daemon
 brew "moreutils"
 brew "openssl"                    # SSL/TLS cryptography library
@@ -33,6 +33,7 @@ brew "sipcalc"                    # Advanced console-based IP subnet calculator
 brew "speedtest-cli"              # Command-line interface for https://speedtest.net bandwidth tests
 brew "sqlite"                     # Command-line interface for SQLite
 brew "svn"                        # Required for source-sans-pro
+brew "switchaudio-osx"
 brew "tylerbrock/saw/saw"         # Fast, multipurpose tool for AWS CloudWatch Logs
 brew "unixodbc"                   # ODBC 3 connectivity for UNIX
 brew "vault"                      # Secures, stores, and tightly controls access to secrets
@@ -45,7 +46,6 @@ cask "font-hack-nerd-font"
 cask "font-source-sans-3"
 cask "google-cloud-sdk"
 cask "wkhtmltopdf"
-cask "1password/tap/1password-cli"
 
 
 instance_eval(File.read("common.rb"))
