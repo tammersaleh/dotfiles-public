95867b453e1c17cefebd236a1ad00da05e850f23
Tammer Saleh <tammer@superorbital.io>
2024-01-14 12:25:04 -0800

    Neovim save and set insert mode when moving in/out of terminal

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -296,10 +296,22 @@ vim.g.loaded_netrwPlugin = 1
 
 -- Splits
 
-vim.keymap.set({'n', 't'}, '<C-h>', function() vim.cmd.wincmd("h") end, {desc = "Move to window to the left."})
-vim.keymap.set({'n', 't'}, '<C-j>', function() vim.cmd.wincmd("j") end, {desc = "Move to window below."})
-vim.keymap.set({'n', 't'}, '<C-k>', function() vim.cmd.wincmd("k") end, {desc = "Move to window above."})
-vim.keymap.set({'n', 't'}, '<C-l>', function() vim.cmd.wincmd("l") end, {desc = "Move to window to the right."})
+local function save_last_mode()
+  vim.b._last_mode = vim.api.nvim_get_mode().mode
+end
+
+local function fn_to_save_mode_and_move(direction_key)
+  local fn = function()
+    save_last_mode()
+    vim.cmd.wincmd(direction_key)
+  end
+  return fn
+end
+
+vim.keymap.set({'n', 't'}, '<C-h>', fn_to_save_mode_and_move("h"), {desc = "Move to window to the left."})
+vim.keymap.set({'n', 't'}, '<C-j>', fn_to_save_mode_and_move("j"), {desc = "Move to window below."})
+vim.keymap.set({'n', 't'}, '<C-k>', fn_to_save_mode_and_move("k"), {desc = "Move to window above."})
+vim.keymap.set({'n', 't'}, '<C-l>', fn_to_save_mode_and_move("l"), {desc = "Move to window to the right."})
 
 local function dirvish_vsplit()
   vim.cmd('vsplit')
@@ -340,6 +352,7 @@ vim.api.nvim_create_autocmd('TermOpen', {
     vim.opt_local.relativenumber = false
     vim.opt_local.number = false
     vim.cmd "startinsert!"
+    save_last_mode()
   end,
   group = vim_term
 })
@@ -352,6 +365,16 @@ vim.api.nvim_create_autocmd('TermClose', {
       if buffer.name == "" and buffer.linecount == 1 and buffer.changed == 0 then
         vim.cmd.qall()
       end
+    end
+  end,
+  group = vim_term
+})
+
+vim.api.nvim_create_autocmd('WinEnter', {
+  pattern = "term://*",
+  callback = function()
+    if vim.b._last_mode == 't' then
+      vim.cmd.startinsert()
     end
   end,
   group = vim_term
