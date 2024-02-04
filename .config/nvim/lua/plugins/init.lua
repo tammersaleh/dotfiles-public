8b1d0900579f29d0affe4f9c60160f80f82cc274
Tammer Saleh <tammer@superorbital.io>
2024-02-04 14:17:03 -0800

    Fix/add plugins

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -18,7 +18,17 @@ return {
       {'ge', '<Plug>(smartword-ge)', mode = {'n', 'o', 'v'}},
     },
   },
-  { "kylechui/nvim-surround", event = "VeryLazy", },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end
+  },
+  { "stevearc/dressing.nvim", event = "VeryLazy" },
 
   'zhimsel/vim-stay',
   'RRethy/vim-illuminate',
