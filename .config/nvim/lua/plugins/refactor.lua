a48beac3390d411edd8d3b248e2a379dcf7060f5
Tammer Saleh <tammer@superorbital.io>
2024-03-02 09:14:44 -0800

    Add refactor vim plugin

---
+++ .config/nvim/lua/plugins/refactor.lua
@@ -0,0 +1,19 @@
+return {
+  'ThePrimeagen/refactoring.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  keys = {
+    {
+       "<leader>rr",
+      function() require('telescope').extensions.refactoring.refactors() end,
+      mode = {'n', 'x'},
+      desc = "[R]efactor"},
+  },
+  config = function()
+    require('refactoring').setup()
+    require('telescope').load_extension('refactoring')
+  end,
+}
