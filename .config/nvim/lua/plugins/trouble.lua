82487cda9a6a304e81d282e31f08b71f025a4c9c
Tammer Saleh <tammer@superorbital.io>
2024-09-27 08:52:50 -0700

    stuff

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,15 +1,37 @@
 return {
   "folke/trouble.nvim",
-
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    padding = false,
-    auto_close = true,
-  },
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
-    {"gt", function() require("trouble").toggle() end, desc = "Toggle Trouble"},
-  }
+    {
+      "gtt",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "gtx",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "gts",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "gtl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "gtL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "gtq",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
