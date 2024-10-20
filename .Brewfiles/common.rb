fdaf142903e8c5c02cffef5bf9c55bc30ddf4b1a
Tammer Saleh <tammer@superorbital.io>
2024-10-20 14:11:50 -0700

    add cli improvements

--- .Brewfiles/common.rb
+++ .Brewfiles/common.rb
@@ -86,8 +86,10 @@ brew 'yq'
 brew 'zlib'                          # General-purpose lossless data-compression library
 brew 'zsh'
 brew 'man-db'                        # Avoid "This manpage is not compatible with mandoc(1) and might display incorrectly" errors
+brew 'zsh-syntax-highlighting'       # Colorized command line prompt
 
 cask 'font-codicon'                  # VSCode font icons
 cask 'google-cloud-sdk'
+cask 'amazon-q'                      # autocomplete magic
 
 # vim: ft=ruby
