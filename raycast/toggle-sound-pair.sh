2dce082533fda022a326c32d4c53cd039b3d6fb7
Tammer Saleh <tammer@superorbital.io>
2024-10-27 15:56:43 -0700

    Track raycast config

---
+++ raycast/toggle-sound-pair.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Toggle Sound Pair
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🎤
+
+# Documentation:
+# @raycast.author Tammer Saleh
+
+~/bin/audio toggle
