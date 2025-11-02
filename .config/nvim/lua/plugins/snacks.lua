c3cb97061b7518337b8999cd6dd33cb11fcd2822
Tammer Saleh <me@tammersaleh.com>
2025-11-02 11:48:17 -0800

    simpler animation

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,7 +2,6 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  ---@type snacks.Config
   opts = {
     input = { enabled = true },
     picker = { enabled = true }, -- people say this using the ivy layout is a good telescope replacement
@@ -12,7 +11,23 @@ return {
     -- notifier = { enabled = true },
     -- quickfile = { enabled = true },
     -- scope = { enabled = true },
-    -- scroll = { enabled = true },
+    scroll = {
+      enabled = true,
+      animate = {
+        duration = { step = 10, total = 200 },
+        easing = "linear",
+      },
+      -- faster animation when repeating scroll after delay
+      animate_repeat = {
+        delay = 100, -- delay in ms before using the repeat animation
+        duration = { step = 5, total = 50 },
+        easing = "linear",
+      },
+      -- what buffers to animate
+      -- filter = function(buf)
+      --   return vim.g.snacks_scroll ~= false and vim.b[buf].snacks_scroll ~= false and vim.bo[buf].buftype ~= "terminal"
+      -- end,
+    },
     -- statuscolumn = { enabled = true },
     -- words = { enabled = true },
   },
