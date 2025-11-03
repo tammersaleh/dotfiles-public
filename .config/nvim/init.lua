2057c90cb54ef93b385326012cdea64ec6cbc3fb
Tammer Saleh <me@tammersaleh.com>
2025-11-02 16:34:17 -0800

    Disable lazy.nvim change detection

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,7 +16,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  change_detection = { enabled = false },
+})
 
 -- [[ Setting options ]]
 
