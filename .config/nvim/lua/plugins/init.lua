41c2390453918c9a07e02748505889b68638280f
Tammer Saleh <me@tammersaleh.com>
2025-11-06 09:31:56 -0800

    stuff

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -50,10 +50,7 @@ return {
   { 'numToStr/Comment.nvim', opts = {}, lazy = false, },
   {
     "zenbones-theme/zenbones.nvim",
-    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-    -- In Vim, compat mode is turned on as Lush only works in Neovim.
-    dependencies = "rktjmp/lush.nvim",
+    dependencies = "rktjmp/lush.nvim", -- Allows for more configuration or extending the colorscheme
     lazy = false,
     priority = 1000,
     -- you can set set configuration options here
