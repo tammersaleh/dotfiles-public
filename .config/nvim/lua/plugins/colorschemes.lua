e92ac949911becbf38a92eb9a87659a9a86244f5
Tammer Saleh <me@tammersaleh.com>
2025-11-13 09:37:46 -0800

    colors

---
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "neanias/everforest-nvim",
+    main = "everforest",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    opts = {
+      background = "hard", -- "soft", "medium", "hard"
+      italics = true,
+      dim_inactive_windows = true,
+      show_eob = false,
+      on_highlights = function(hl, palette)
+        hl.Comment = { fg = palette.yellow, bg = palette.none, italic = true }
+        hl.String = { fg = palette.white, bg = palette.none, italic = true }
+      end,
+      colours_override = function (palette)
+        palette.white = "#000000"
+      end
+    },
+  },
+  -- Add a dummy plugin to set the colorscheme after all are loaded
+  {
+    "set-colorscheme",
+    lazy = false,
+    priority = 999, -- Lower priority than colorscheme plugins
+    dir = vim.fn.stdpath("config"), -- Point to a valid directory
+    config = function()
+      vim.cmd.colorscheme("everforest")
+      vim.api.nvim_set_hl(0, 'Terminal',    { bg = 'black' }) -- Example dark background
+      vim.api.nvim_set_hl(0, 'TerminalWin', { bg = 'black' }) -- Also for the terminal window itself
+      vim.api.nvim_set_hl(0, 'Comment', { fg = 'white' })
+      vim.api.nvim_set_hl(0, 'TSString', { fg = 'white' })
+    end,
+  },
+}
