f5c323199cd12d7f530239a73773fb004df3add6
Tammer Saleh <me@tammersaleh.com>
2025-11-18 17:07:23 -0800

    remove unused stuff

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,3 +1,13 @@
+local config = function()
+  vim.cmd.colorscheme("everforest")
+  -- vim.cmd.colorscheme("default")
+  -- vim.api.nvim_set_hl(0, 'Terminal',    { bg = 'black' }) -- Example dark background
+  -- vim.api.nvim_set_hl(0, 'TerminalWin', { bg = 'black' }) -- Also for the terminal window itself
+  vim.api.nvim_set_hl(0, 'Comment', { fg = 'white', bold = true })
+  -- vim.api.nvim_set_hl(0, 'TSString', { fg = 'white' })
+  vim.api.nvim_set_hl(0, 'String', { fg = 'white' })
+end
+
 return {
   {
     "neanias/everforest-nvim",
@@ -10,13 +20,13 @@ return {
       italics = true,
       dim_inactive_windows = true,
       show_eob = false,
-      on_highlights = function(hl, palette)
-        hl.Comment = { fg = palette.yellow, bg = palette.none, italic = true }
-        hl.String = { fg = palette.white, bg = palette.none, italic = true }
-      end,
-      colours_override = function (palette)
-        palette.white = "#000000"
-      end
+      -- on_highlights = function(hl, palette)
+      --   hl.Comment = { fg = palette.yellow, bg = palette.none, italic = true }
+      --   hl.String = { fg = palette.white, bg = palette.none, italic = true }
+      -- end,
+      -- colours_override = function (palette)
+      --   palette.white = "#000000"
+      -- end
     },
   },
   -- Add a dummy plugin to set the colorscheme after all are loaded
@@ -25,13 +35,6 @@ return {
     lazy = false,
     priority = 999, -- Lower priority than colorscheme plugins
     dir = vim.fn.stdpath("config"), -- Point to a valid directory
-    config = function()
-      vim.cmd.colorscheme("everforest")
-      vim.api.nvim_set_hl(0, 'Terminal',    { bg = 'black' }) -- Example dark background
-      vim.api.nvim_set_hl(0, 'TerminalWin', { bg = 'black' }) -- Also for the terminal window itself
-      vim.api.nvim_set_hl(0, 'Comment', { fg = 'white' })
-      vim.api.nvim_set_hl(0, 'TSString', { fg = 'white' })
-      vim.api.nvim_set_hl(0, 'String', { fg = 'white' })
-    end,
+    config = config,
   },
 }
